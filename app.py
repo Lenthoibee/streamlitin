3799fd9cdc3da21af8fb927240c5cb57f116db7d
Lenthoibee <156999969+Lenthoibee@users.noreply.github.com>
2024-01-23 18:46:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model=genai.GenerativeModel("gemini-pro")
 chat=model.start_chat(history=[])
 
 def get_gemini_response(question):
-    response=chat.send_message(question,stream=true)
+    response=chat.send_message(question,stream=TRUE)
     return response
 
 st.set_page_config("Q&A Chatbot")
