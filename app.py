fc080fbdae13db7189759f1b470f3f10f8c0bc8f
Lenthoibee <156999969+Lenthoibee@users.noreply.github.com>
2024-01-23 18:18:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import google.generativeai as genai
+import streamlit as st
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model=genai.GenerativeMode1("gemini.pro")
+chat=genai.start_chat(history=[])
+
+def get_gemini_response(question):
+    response=chat.send_message(question,stream=true)
+    return response
+
+st.set_page_config("Q&A Chatbot")
+st.header("Conversational ChatBot")
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input=st.txt_input("Input: ", key="input")
+submit=st.button("Send")
+
+if input and submit:
+    response=get_gemini_response(input)
+    st.session_state['chat_history'],append(("You",input))
+    st.subheader("Response: ")
+    for chunk in response:
+        st.write(chunk.txt)
+        st.session_state['chat_history'].append(("Response", chunk.text))
+
+st.subheader("Chat history: ")
+for role, response in st.session__state["chat_history"]:
+    st.write(f"{role}: {response}")
+
+
+
+
+
+
